7cf77a36670fa7207d1bddec6fcd92a72ac74573
AoTarius <1836411990@qq.com>
2026-03-19 16:39:51 +0800

    修复了对比分析显示内容不是完全来自数据库的小bug

--- frontend/src/views/CompareView.vue
+++ frontend/src/views/CompareView.vue
@@ -59,7 +59,7 @@ defineExpose({
           >
             <div class="item-info">
               <div class="item-label">{{ index === 0 ? '记录 A' : '记录 B' }}</div>
-              <div class="item-name">{{ record.video_name }}</div>
+              <div class="item-name">{{ record.task_name }}</div>
               <div class="item-details">
                 <span class="detail-item">
                   <svg
@@ -76,7 +76,7 @@ defineExpose({
                       d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"
                     ></path>
                   </svg>
-                  {{ record.result?.model_name || 'N/A' }}
+                  {{ record.model_name || 'N/A' }}
                 </span>
               </div>
             </div>
